767e65f169b8d6931ddd2a51024f3332c6fc64b2
Shelly Bensal <shelly.bensal@gmail.com>
2018-05-06 12:23:49 -0400

    Beginning data exploration.

--- testing.py
+++ testing.py
@@ -1,30 +1,32 @@
-from load_data import get_all_data
+from load_data import get_movies
 from sklearn.svm import SVC
 import pandas as pd
 import numpy as np
 
-df = get_all_data()
-print(df.shape)
+df = get_movies()
 
-train = df.sample(frac=0.9, random_state = 200)
-test = df.drop(train.index)
+train = df.sample(frac=0.8, random_state = 200).dropna()
+test = df.drop(train.index).dropna()
 
-X_train = pd.to_numeric(train['Revenue'], errors='coerce').fillna(0).astype(np.int64)
-y_train = pd.to_numeric(train['Bechdel_Rating'], errors='coerce').fillna(0).astype(np.int64)
+features = ['Revenue', 'Popularity', 'Budget', 'Year']
 
-X_test = pd.to_numeric(test['Revenue'], errors='coerce').fillna(0).astype(np.int64)
-y_test = pd.to_numeric(test['Bechdel_Rating'], errors='coerce').fillna(0).astype(np.int64)
+X_train = train[features].apply(pd.to_numeric,  errors='coerce').astype(np.int64)
+y_train = pd.to_numeric(train['Bechdel_Rating'], errors='coerce').astype(np.int64)
+
+X_test = test[features].apply(pd.to_numeric,  errors='coerce').astype(np.int64)
+y_test = pd.to_numeric(test['Bechdel_Rating'], errors='coerce').astype(np.int64)
 
 
-X_train = X_train.reshape(X_train.shape[0], 1)
-X_test = X_test.reshape(X_test.shape[0], 1)
+# X_train = X_train.reshape(X_train.shape[0], 1)
+# X_test = X_test.reshape(X_test.shape[0], 1)
 print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
 clf = SVC(kernel='linear')
 clf.fit(X_train, y_train)
-print(clf.score(X_test, y_test))
+print("Training accuracy: ", clf.score(X_train, y_train))
+print("Testing accuracy: ", clf.score(X_test, y_test))
 
-print(sum(y_test == np.zeros(y_test.shape[0]))/y_test.shape[0])
-print(sum(y_test == np.ones(y_test.shape[0]))/y_test.shape[0])
-print(sum(y_test == (2*np.ones(y_test.shape[0])))/y_test.shape[0])
-print(sum(y_test == (3*np.ones(y_test.shape[0])))/y_test.shape[0])
+print("Zero: ", sum(y_test == np.zeros(y_test.shape[0]))/y_test.shape[0])
+print("One: ", sum(y_test == np.ones(y_test.shape[0]))/y_test.shape[0])
+print("Two: ", sum(y_test == (2*np.ones(y_test.shape[0])))/y_test.shape[0])
+print("Three: ", sum(y_test == (3*np.ones(y_test.shape[0])))/y_test.shape[0])
