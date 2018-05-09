804c09972ebee9f90ede753794d58800b7cfdfa2
nchatter <nchatter@andrew.cmu.edu>
2018-05-08 21:16:36 -0400

    Adding model code

--- testing.py
+++ testing.py
@@ -1,6 +1,9 @@
 from load_data import get_movies
 import feature_engineering as fe 
 from sklearn.svm import SVC
+from sklearn import tree
+from sklearn import mixture
+from sklearn.naive_bayes import GaussianNB
 import pandas as pd
 import numpy as np
 
@@ -22,7 +25,10 @@ features = ['Female_Dir_Score',
 			'Cast_Pop',
 			]
 
+#### SVM ######
+print("SVM")
 df = df.dropna()
+
 train = df.sample(frac=0.8, random_state = 200)
 test = df.drop(train.index)
 
@@ -32,11 +38,9 @@ y_train = pd.to_numeric(train['Bechdel_Rating'], errors='coerce').astype(float)
 X_test = test[features].apply(pd.to_numeric,  errors='coerce').astype(float)
 y_test = pd.to_numeric(test['Bechdel_Rating'], errors='coerce').astype(float)
 
-
-# X_train = X_train.reshape(X_train.shape[0], 1)
-# X_test = X_test.reshape(X_test.shape[0], 1)
 print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
+print("SVM rbf kernel")
 clf = SVC(kernel='rbf')
 clf.fit(X_train, y_train)
 print("Training accuracy: ", clf.score(X_train, y_train))
@@ -46,3 +50,101 @@ print("Zero: ", sum(y_test == np.zeros(y_test.shape[0]))/y_test.shape[0])
 print("One: ", sum(y_test == np.ones(y_test.shape[0]))/y_test.shape[0])
 print("Two: ", sum(y_test == (2*np.ones(y_test.shape[0])))/y_test.shape[0])
 print("Three: ", sum(y_test == (3*np.ones(y_test.shape[0])))/y_test.shape[0])
+
+print("#################")
+
+print("SVM linear kernel")
+clf = SVC(kernel='linear')
+clf.fit(X_train, y_train)
+print("Training accuracy: ", clf.score(X_train, y_train))
+print("Testing accuracy: ", clf.score(X_test, y_test))
+
+print("Zero: ", sum(y_test == np.zeros(y_test.shape[0]))/y_test.shape[0])
+print("One: ", sum(y_test == np.ones(y_test.shape[0]))/y_test.shape[0])
+print("Two: ", sum(y_test == (2*np.ones(y_test.shape[0])))/y_test.shape[0])
+print("Three: ", sum(y_test == (3*np.ones(y_test.shape[0])))/y_test.shape[0])
+
+print("#################")
+
+##### Decision Tree #######
+print("Decision Tree")
+
+train = df.sample(frac=0.8, random_state = 200)
+test = df.drop(train.index)
+
+X_train = train[features].apply(pd.to_numeric,  errors='coerce').astype(float)
+y_train = pd.to_numeric(train['Bechdel_Rating'], errors='coerce').astype(float)
+
+X_test = test[features].apply(pd.to_numeric,  errors='coerce').astype(float)
+y_test = pd.to_numeric(test['Bechdel_Rating'], errors='coerce').astype(float)
+
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+print("DT Max Depth 4")
+clf = tree.DecisionTreeClassifier(max_depth=4)
+clf.fit(X_train, y_train)
+print("Training accuracy: ", clf.score(X_train, y_train))
+print("Testing accuracy: ", clf.score(X_test, y_test))
+
+print("Zero: ", sum(y_test == np.zeros(y_test.shape[0]))/y_test.shape[0])
+print("One: ", sum(y_test == np.ones(y_test.shape[0]))/y_test.shape[0])
+print("Two: ", sum(y_test == (2*np.ones(y_test.shape[0])))/y_test.shape[0])
+print("Three: ", sum(y_test == (3*np.ones(y_test.shape[0])))/y_test.shape[0])
+
+print("#################")
+
+print("DT Max Depth 3")
+clf = tree.DecisionTreeClassifier(max_depth=3)
+clf.fit(X_train, y_train)
+print("Training accuracy: ", clf.score(X_train, y_train))
+print("Testing accuracy: ", clf.score(X_test, y_test))
+
+print("Zero: ", sum(y_test == np.zeros(y_test.shape[0]))/y_test.shape[0])
+print("One: ", sum(y_test == np.ones(y_test.shape[0]))/y_test.shape[0])
+print("Two: ", sum(y_test == (2*np.ones(y_test.shape[0])))/y_test.shape[0])
+print("Three: ", sum(y_test == (3*np.ones(y_test.shape[0])))/y_test.shape[0])
+
+print("#################")
+
+##### GMM #######
+print("GMM")
+train = df.sample(frac=0.8, random_state = 200)
+test = df.drop(train.index)
+
+X_train = train[features].apply(pd.to_numeric,  errors='coerce').astype(float)
+y_train = pd.to_numeric(train['Bechdel_Rating'], errors='coerce').astype(float)
+
+X_test = test[features].apply(pd.to_numeric,  errors='coerce').astype(float)
+y_test = pd.to_numeric(test['Bechdel_Rating'], errors='coerce').astype(float)
+
+clf = mixture.GMM(n_components=4)
+clf.fit(X_train)
+print("GMM score")
+print(clf.score_samples(X_test))
+
+print("#################")
+
+##### Naive Bayes #######
+print("Naive Bayes")
+train = df.sample(frac=0.8, random_state = 200)
+test = df.drop(train.index)
+
+X_train = train[features].apply(pd.to_numeric,  errors='coerce').astype(float)
+y_train = pd.to_numeric(train['Bechdel_Rating'], errors='coerce').astype(float)
+
+X_test = test[features].apply(pd.to_numeric,  errors='coerce').astype(float)
+y_test = pd.to_numeric(test['Bechdel_Rating'], errors='coerce').astype(float)
+
+clf = GaussianNB()
+clf.fit(X_train, y_train)
+
+print("Training accuracy: ", clf.score(X_train, y_train))
+print("Testing accuracy: ", clf.score(X_test, y_test))
+
+print("Zero: ", sum(y_test == np.zeros(y_test.shape[0]))/y_test.shape[0])
+print("One: ", sum(y_test == np.ones(y_test.shape[0]))/y_test.shape[0])
+print("Two: ", sum(y_test == (2*np.ones(y_test.shape[0])))/y_test.shape[0])
+print("Three: ", sum(y_test == (3*np.ones(y_test.shape[0])))/y_test.shape[0])
+
+print("#################")
+
